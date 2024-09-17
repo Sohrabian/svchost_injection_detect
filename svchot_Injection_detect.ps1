a4f1ca72ad48e2a8abe5d2fede6ce1a93983369a
BlueTrust <92355496+Sohrabian@users.noreply.github.com>
2024-09-17 11:40:56 +0330

    uploaded SVCHOST Injection detection powershell

---
+++ svchot_Injection_detect.ps1
@@ -0,0 +1,15 @@
+﻿$svc = Get-Item 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Svchost\'
+$svc_export = foreach ($a in $svc.Property) {
+	
+	[PSCustomObject]@{    ##using the pscustomeobject we define a table fields Name, Value of The Name. actually the Name reffered to the Service Group
+		Name = $a
+		Value = $svc.Getvalue($a)
+	}
+
+#$svc_export
+#$svc_export.Value
+
+$final_value = (compare-object -Referenceobject ($svc_export.value) -Differenceobject (Get-Content -Path '.\svchost (2).txt'))
+$final_value
+
+}
